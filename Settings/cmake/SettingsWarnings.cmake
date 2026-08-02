d774d54c57b1959b60cf9a682959513ce8125983
Agony <aaritsuyu@gmail.com>
2026-08-02 16:07:40 -0300

    build(settings): add native foundation target

---
+++ Settings/cmake/SettingsWarnings.cmake
@@ -0,0 +1,13 @@
+function(settings_enable_warnings target)
+    if(MSVC)
+        target_compile_options(${target} PRIVATE /W4 /permissive-)
+    else()
+        target_compile_options(${target} PRIVATE
+            -Wall
+            -Wextra
+            -Wpedantic
+            -Wconversion
+            -Wsign-conversion
+        )
+    endif()
+endfunction()
